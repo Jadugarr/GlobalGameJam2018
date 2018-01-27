fb2d1181477232872c3a40213e747b43ef1ba6e5
Kim Mannstedt <k.mannstedt@gmail.com>
2018-01-27 20:45:28 +0100

    fix highlight bug

--- Assets/Scripts/HighlightObject.cs
+++ Assets/Scripts/HighlightObject.cs
@@ -4,26 +4,9 @@ using UnityEngine;
 
 public class HighlightObject : MonoBehaviour {
 
-	public bool highlighted = false;
-
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
 	void Update () {
 
 		Shader defaultShader = Shader.Find ("Standard");
-		Shader highlightShader = Shader.Find ("Legacy Shaders/Self-Illumin/Diffuse");
-
-		if (highlighted) {
-			transform.GetComponent<Renderer> ().material.shader = highlightShader;
-		} else {
-			transform.GetComponent<Renderer> ().material.shader = defaultShader;
-		}
-	}
-
-	void LateUpdate(){
-		highlighted = false;
+		transform.GetComponent<Renderer> ().material.shader = defaultShader;
 	}
 }
