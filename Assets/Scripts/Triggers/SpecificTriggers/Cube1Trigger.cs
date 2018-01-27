3c77e5396b64d8783947f45e58802da939e1cc65
Kim Mannstedt <k.mannstedt@gmail.com>
2018-01-27 14:27:17 +0100

    added trigger for testing

---
+++ Assets/Scripts/Triggers/SpecificTriggers/Cube1Trigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Triggers.SpecificTriggers{
+public class Cube1Trigger : TriggerComponent {
+
+		public override void Activate()
+        {
+			base.Activate();
+        }
+
+        public override void Deactivate()
+        {
+            base.Deactivate();
+        }
+
+        protected override void AddEventListeners()
+        {
+            eventManager.RegisterForEvent(EventTypes.SendTerminalInput, onCubeInput);
+        }
+
+        protected override void RemoveEventListeners()
+        {
+            eventManager.RemoveFromEvent(EventTypes.SendTerminalInput, onCubeInput);
+        }
+
+		private void onCubeInput(IEvent data){
+			if(Input.GetKey(KeyCode.Q)){
+				Debug.Log("'Q' pressed");
+				eventManager.FireEvent(EventTypes.TriggerActivated, new TriggerActivatedEvent(TriggerId));
+			}
+		}
+}
+}
