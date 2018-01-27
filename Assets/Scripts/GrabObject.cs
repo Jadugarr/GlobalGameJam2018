35a6211839e1dd7e62a537fd8fff520d49ab1415
Kim Mannstedt <k.mannstedt@gmail.com>
2018-01-27 01:36:27 +0100

    added movement and interaction

---
+++ Assets/Scripts/GrabObject.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GrabObject : MonoBehaviour {
+
+	public int viewDistance;
+	public LayerMask layer;
+	public int rotateSpeed;
+	public GameObject player;
+
+	private bool objectHold;
+	private GameObject currentObject;
+	private Vector3 objectPosition;
+	private Quaternion objectRotation;
+	private bool mouseReleased;
+
+	void Start(){
+		objectHold = false;
+		mouseReleased = true;
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown (0)) {
+			mouseReleased = true;
+		}
+	}
+
+	void FixedUpdate(){
+
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit = new RaycastHit ();
+
+		if (Physics.Raycast (transform.position, forward, out hit, viewDistance, layer) && !objectHold) {
+			hit.transform.gameObject.GetComponent<HighlightObject>().highlighted = true;
+			if (Input.GetMouseButtonDown (0) && mouseReleased) {
+				player.transform.gameObject.GetComponent<RigidbodyFirstPersonController> ().LockMovement();
+				mouseReleased = false;
+				objectHold = true;
+				currentObject = hit.transform.gameObject;
+				objectPosition = currentObject.transform.position;
+				objectRotation = currentObject.transform.rotation;
+				HoldObject ();
+			}
+		}
+
+		if(objectHold){
+			HoldObject ();
+
+			//rotate object when q or e pressed
+			/*
+			if (Input.GetKey (KeyCode.Q)) {
+				currentObject.transform.Rotate (Vector3.up * rotateSpeed);
+			}
+
+			if (Input.GetKey (KeyCode.E)) {
+				currentObject.transform.Rotate (-Vector3.up * rotateSpeed);
+			}*/
+
+			currentObject.transform.Rotate (new Vector3(-Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
+
+			if (Input.GetMouseButtonDown (0) && mouseReleased) {
+				player.transform.gameObject.GetComponent<RigidbodyFirstPersonController> ().UnlockMovement ();
+				mouseReleased = false;
+				objectHold = false;
+				currentObject.transform.position = objectPosition;
+				currentObject.transform.rotation = objectRotation;
+				currentObject = null;
+			}
+		}
+
+	}
+
+	void HoldObject(){
+		currentObject.transform.position = transform.position + transform.forward;
+	}
+
+	void HighlightObject(){
+		
+	}
+}
---
+++ Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour {
+
+	public bool highlighted = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Shader defaultShader = Shader.Find ("Standard");
+		Shader highlightShader = Shader.Find ("Legacy Shaders/Self-Illumin/Diffuse");
+
+		if (highlighted) {
+			transform.GetComponent<Renderer> ().material.shader = highlightShader;
+		} else {
+			transform.GetComponent<Renderer> ().material.shader = defaultShader;
+		}
+	}
+
+	void LateUpdate(){
+		highlighted = false;
+	}
+}
