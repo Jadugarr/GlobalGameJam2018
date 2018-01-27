9303c41ad69200a676563275d5f3aa6f3ea068c0
Kristof Slowikowski <kristof@slowikowski.net>
2018-01-27 03:40:33 +0100

    Clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+
+    public TextMesh textMesh;
+    public float timeLeft;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    timeLeft = 60 * 3 + 5;
+	    textMesh.text = "00:00";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    timeLeft -= Time.deltaTime;
+
+	    int minutes = (int) timeLeft / 60;
+	    int seconds = (int) (timeLeft - (minutes * 60));
+
+	    textMesh.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+	}
+}
