1ec659d34028b575f4e8afcd90ed3fb732f3edf1
Kim Mannstedt <k.mannstedt@gmail.com>
2018-01-27 14:59:12 +0100

    corrected trigger

--- Assets/Scripts/Triggers/SpecificTriggers/Cube1Trigger.cs
+++ Assets/Scripts/Triggers/SpecificTriggers/Cube1Trigger.cs
@@ -2,33 +2,33 @@
 using Events;
 
 namespace Triggers.SpecificTriggers{
-public class Cube1Trigger : TriggerComponent {
+	public class Cube1Trigger : TriggerComponent {
 
-		public override void Activate()
-        {
-			base.Activate();
-        }
-
-        public override void Deactivate()
-        {
-            base.Deactivate();
-        }
-
-        protected override void AddEventListeners()
-        {
-            eventManager.RegisterForEvent(EventTypes.SendTerminalInput, onCubeInput);
-        }
-
-        protected override void RemoveEventListeners()
-        {
-            eventManager.RemoveFromEvent(EventTypes.SendTerminalInput, onCubeInput);
-        }
-
-		private void onCubeInput(IEvent data){
-			if(Input.GetKey(KeyCode.Q)){
-				Debug.Log("'Q' pressed");
-				eventManager.FireEvent(EventTypes.TriggerActivated, new TriggerActivatedEvent(TriggerId));
+			public override void Activate()
+			{
+				base.Activate();
 			}
-		}
-}
+
+			public override void Deactivate()
+			{
+				base.Deactivate();
+			}
+
+			protected override void AddEventListeners()
+			{
+				base.AddEventListeners();
+			}
+
+			protected override void RemoveEventListeners()
+			{
+				base.AddEventListeners();
+			}
+
+			void Update(){
+				if(Input.GetKey(KeyCode.Q)){
+					Debug.Log("'Q' pressed");
+					eventManager.FireEvent(EventTypes.TriggerActivated, new TriggerActivatedEvent(TriggerId));
+				}
+			}
+	}
 }
