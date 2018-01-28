7cc6066c87b8f160a7a0c41115e53200ad4ae813
René Löscher <reneloescher@gmx.net>
2018-01-28 14:17:22 +0100

    ending trigger and open door animation

---
+++ Assets/Scripts/Components/OpenDoorComponent.cs
@@ -0,0 +1,18 @@
+﻿using Events;
+using UnityEngine;
+
+public class OpenDoorComponent : MonoBehaviour
+{
+    public Animator DoorAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        EventManager.Instance.RegisterForEvent(EventTypes.AllTriggersActivated, OnLastTrigger);
+    }
+
+    private void OnLastTrigger(IEvent evtData)
+    {
+        DoorAnimator.SetBool("Open", true);
+    }
+}
