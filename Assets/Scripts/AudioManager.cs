9e15aa08ccf6e832692d28fb3b3f4ebe329c9604
Kristof Slowikowski <kristof@slowikowski.net>
2018-01-28 01:23:15 +0100

    Changes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip buttonSound;
+    public AudioClip spookySound;
+
+    public AudioSource behindPlayerSource;
+    public AudioSource inFrontOfPlayerSource;
+
+    private static AudioManager instance = new AudioManager();
+    private EventManager eventManager = EventManager.Instance;
+
+    // Singleton instance
+    public static AudioManager Instance
+    {
+        get
+        {
+            return AudioManager.instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+	{
+        PlaySpookyPlayerSound();
+        eventManager.RegisterForEvent(EventTypes.AllTriggersActivated, PlayFinalSound);
+	}
+
+    public void PlayButtonSound()
+    {
+        inFrontOfPlayerSource.clip = buttonSound;
+        inFrontOfPlayerSource.Play();
+    }
+
+    public void PlaySpookyPlayerSound()
+    {
+        behindPlayerSource.clip = spookySound;
+        behindPlayerSource.Play();
+    }
+
+    public void PlayFinalSound(IEvent e)
+    {
+        Debug.Log("TODO: Play final sound!");
+    }
+}
---
+++ Assets/Scripts/FinalDoorEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+public class FinalDoorEvent : MonoBehaviour {
+
+    private EventManager eventManager = EventManager.Instance;
+
+    // Use this for initialization
+    void Start () {
+        eventManager.RegisterForEvent(EventTypes.AllTriggersActivated, OnFinalTrigger);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnFinalTrigger(IEvent evtData)
+    {
+        GameObject.Find("Door").GetComponent<Door>().state = DoorState.Opening;
+    }
+}
